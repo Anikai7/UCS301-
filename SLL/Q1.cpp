8cb35deea185609bf6ee5892fd6fb776d92fde72
Anikai7 <anikait749@gmail.com>
2025-01-09 02:19:54 -0800

    things

---
+++ SLL/Q1.cpp
@@ -0,0 +1,22 @@
+#include<iostream>
+
+using namespace std;
+
+int main(){
+
+    cout<<"1.Insert Beginning"<<endl
+        <<"2.Insert Ending"<<endl
+        <<"3.Insert Between"<<endl
+        <<"4.Delete Beginning"<<endl
+        <<"5.Delete End"<<endl
+        <<"6.Delete Specific Node"<<endl
+        <<"7.Search"<<endl
+        <<"8.Display"<<endl
+        <<"9.End"<<endl;
+
+    int n;
+    cin>>n;
+
+    
+    return 0;
+}
